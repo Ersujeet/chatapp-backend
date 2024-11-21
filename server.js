55fe90ad8d6dc8b0d66e669d2011c8d17dc7e83b
Sujeet Kumar <sujeetmaurya8419@gmail.com>
2024-11-21 10:52:57 +0530

    Update server.js
    Cors update

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(express.json());
 
 // app.use(cors());
+app.use(cors({
+  origin: [""],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
